f2bbba330deea595f3d9b4f8068e5c05c6ce9df3
Lautaro <lautaroyanequine@gmail.com>
2022-06-02 23:53:31 -0300

    Comienzo TPF

---
+++ TrabajoFinal/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: lauta
+ * Fecha: 02/06/2022
+ * Hora: 23:48
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+
+namespace TrabajoFinal
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
